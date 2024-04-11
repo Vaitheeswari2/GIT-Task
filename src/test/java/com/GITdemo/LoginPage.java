3828fee31d3264cf620250c66c90e97b4af852a3
Vaitheeswari2 <vaithumano94@gmail.com>
2024-04-12 02:03:43 +0530

    GroceryLogin

--- src/test/java/com/GITdemo/LoginPage.java
+++ src/test/java/com/GITdemo/LoginPage.java
@@ -13,8 +13,22 @@ public class LoginPage {
 	
 	private void login() {
 		System.out.println("Login");
+		
+	}
+	private void state() {
+		System.out.println("Tamil Nadu");	
 
 	}
+	
+	private void country() {
+		
+		System.out.println("India");
+	}
+	private void city() {
+		
+		System.out.println("Chennai");
+	}
+	
 
 	}
 
