aeccddcbf9cf47562f3dadf80e77b1a1f8cbfd0d
Vaitheeswari2 <vaithumano94@gmail.com>
2024-04-11 23:33:47 +0530

    login

---
+++ src/test/java/com/GITdemo/LoginPage.java
@@ -0,0 +1,21 @@
+package com.GITdemo;
+
+public class LoginPage {
+
+	private void username() {
+	System.out.println("User Name");
+
+	}	
+	
+	private void password() {
+		System.out.println("Password");
+	}
+	
+	private void login() {
+		System.out.println("Login");
+
+	}
+
+	}
+
+
